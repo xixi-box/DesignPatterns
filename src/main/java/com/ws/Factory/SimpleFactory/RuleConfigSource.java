645b0c3ec8e8db4287aa76440db9a22139443c0f
ws <2606209307@qq.com>
2023-05-03 11:44:40 +0800

    工厂模式上-简单工厂模式

---
+++ src/main/java/com/ws/Factory/SimpleFactory/RuleConfigSource.java
@@ -0,0 +1,82 @@
+package com.ws.Factory.SimpleFactory;
+
+/**
+ * @Author: 王顺
+ * @Date: 2023/5/3 10:58
+ * @Version 1.0
+ */
+public class RuleConfigSource {
+
+    public RuleConfig load(String ruleConfigFilePath) {
+        //1.根据配置路径选择获取扩展名
+        String ruleConfigFileExtension = getFileExtension(ruleConfigFilePath);
+        //2.创建相对应的工厂实例
+        IRuleConfigParser parser = RuleConfigParserFactory.createParser(ruleConfigFileExtension);
+        if (parser == null) {
+            throw new InvalidRuleConfigException(
+                    "Rule config file format is not supported: " + ruleConfigFilePath);
+        }
+        String configText = "";
+        //从ruleConfigFilePath文件中读取配置文本到configText中
+        RuleConfig ruleConfig = parser.parse(configText);
+        return ruleConfig;
+    }
+
+    private String getFileExtension(String filePath) {
+        //...解析文件名获取扩展名，比如rule.json，返回json
+        return "json";
+    }
+
+}
+class RuleConfigParserFactory {
+    public static IRuleConfigParser createParser(String configFormat) {
+        IRuleConfigParser parser = null;
+        if ("json".equalsIgnoreCase(configFormat)) {
+            parser = new JsonRuleConfigParser();
+        } else if ("xml".equalsIgnoreCase(configFormat)) {
+            parser = new XmlRuleConfigParser();
+        } else if ("yaml".equalsIgnoreCase(configFormat)) {
+            parser = new YamlRuleConfigParser();
+        } else if ("properties".equalsIgnoreCase(configFormat)) {
+            parser = new PropertiesRuleConfigParser();
+        }
+        return parser;
+    }
+}
+
+class RuleConfig {
+    String configText;
+    public RuleConfig(String configText) {
+        this.configText = configText;
+    }
+}
+
+class IRuleConfigParser {
+
+    public RuleConfig parse(String configText) {
+        return new RuleConfig(configText);
+    }
+}
+
+class JsonRuleConfigParser extends RuleConfigParser {
+
+}
+
+class PropertiesRuleConfigParser extends RuleConfigParser {
+
+}
+
+class XmlRuleConfigParser extends RuleConfigParser {
+
+}
+
+class YamlRuleConfigParser extends RuleConfigParser {
+}
+
+class InvalidRuleConfigException extends RuntimeException {
+    String message;
+
+    public InvalidRuleConfigException(String message) {
+        this.message = message;
+    }
+}
---
+++ src/main/java/com/ws/Factory/SimpleFactory/RuleConfigSourceAndSingleton.java
@@ -0,0 +1,55 @@
+package com.ws.Factory.SimpleFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author: 王顺
+ * @Date: 2023/5/3 11:34
+ * @Version 1.0
+ */
+public class RuleConfigSourceAndSingleton {
+    public RuleConfig load(String ruleConfigFilePath) {
+        //1.根据配置路径选择获取扩展名
+        String ruleConfigFileExtension = getFileExtension(ruleConfigFilePath);
+        //2.创建相对应的工厂实例
+        IRuleConfigParser parser = RuleConfigParserFactory.createParser(ruleConfigFileExtension);
+        if (parser == null) {
+            throw new InvalidRuleConfigException(
+                    "Rule config file format is not supported: " + ruleConfigFilePath);
+        }
+        String configText = "";
+        //从ruleConfigFilePath文件中读取配置文本到configText中
+        RuleConfig ruleConfig = parser.parse(configText);
+        return ruleConfig;
+    }
+
+    private String getFileExtension(String filePath) {
+        //...解析文件名获取扩展名，比如rule.json，返回json
+        return "json";
+    }
+
+}
+
+class RuleConfigParserFactoryAndSingleton {
+    private static final Map<String, RuleConfigParser> cachedParsers = new HashMap<>();
+
+    static {
+        cachedParsers.put("json", new JsonRuleConfigParser());
+        cachedParsers.put("xml", new XmlRuleConfigParser());
+        cachedParsers.put("yaml", new YamlRuleConfigParser());
+        cachedParsers.put("properties", new PropertiesRuleConfigParser());
+    }
+
+    public static IRuleConfigParser createParser(String configFormat) {
+        if (configFormat == null || configFormat.isEmpty()) {
+            return null;//返回null还是IllegalArgumentException全凭你自己说了算
+        }
+        IRuleConfigParser parser = cachedParsers.get(configFormat.toLowerCase());
+        return parser;
+    }
+}
+
+class RuleConfigParser extends IRuleConfigParser {
+
+}
